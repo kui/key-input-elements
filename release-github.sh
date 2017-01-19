d196dde4448c2fbb6ad26dfcd0e583c392136aef
Keiichiro Ui <keiichiro.ui@gmail.com>
2017-01-19 14:03:59 +0900

    Fix script

--- release-github.sh
+++ release-github.sh
@@ -2,13 +2,14 @@
 
 set -eux
 
+GH_USER="kui"
+REPO="key-input-elements"
+
 get_version() {
   python -c 'import json; print json.load(open("package.json"))["version"]'
 }
-
-REPO="key-input-elements"
 TAG="v$(get_version)"
-TAR_GZ="key-input-elements-${TAG}.tar.gz"
+TAR_GZ="${REPO}-${TAG}.tar.gz"
 
 rm -fr dist
 
@@ -20,7 +21,7 @@ npm run prod-dist
 
 # Require https://github.com/aktau/github-release
 
-GH_REL_OPTS="--user kui --repo '$REPO' --tag '$TAG'"
+GH_REL_OPTS="--user $GH_USER --repo $REPO --tag $TAG"
 
 github-release release $GH_REL_OPTS \
                --draft
@@ -29,4 +30,4 @@ github-release upload $GH_REL_OPTS \
                --file "dist/$TAR_GZ" \
                --name "$TAR_GZ"
 
-echo "See "
+echo "See https://github.com/$GH_USER/$REPO/releases"
